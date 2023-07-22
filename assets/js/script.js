44a85ee2885b8c76749f920634b46103ca2f6653
francesca-pizzighini <133757414+francesca-pizzighini@users.noreply.github.com>
2023-07-22 13:38:52 +0200

    functioning color changer and right js. i'm proud

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-let blue = document.querySelector('.blue');
+let blue = document.querySelector('.blue')
 let green = document.querySelector('.green')
 let pink = document.querySelector('.pink');
 let black = document.querySelector('.black');
@@ -9,76 +9,25 @@ let orange = document.querySelector('.orange');
 
 
 
-function changeBlue(){
-    document.querySelector('.background-gradient').className = 'blue background-gradient'
-}
-blue.addEventListener('click', changeBlue)
-
-function changeGreen() {
-    document.querySelector('.background-gradient').className = 'green background-gradient'
-}
-green.addEventListener('click', changeGreen)
-
-function changePink() {
-    document.querySelector('.background-gradient').className = 'pink background-gradient'
-}
-pink.addEventListener('click', changePink)
-
-function changeBlack() {
-    document.querySelector('.background-gradient').className = 'black background-gradient'
-}
-black.addEventListener('click', changeBlack)
-
-function changeWhite() {
-    document.querySelector('.background-gradient').className = 'white background-gradient'
-}
-white.addEventListener('click', changeWhite)
 
 
-function changeBeige() {
-    document.querySelector('.background-gradient').className = 'beige background-gradient'
-}
-beige.addEventListener('click', changeBeige)
+let colors = [
+    [blue, 'blue'], 
+    [green, 'green'], 
+    [pink, 'pink'],
+    [black, 'black'],
+    [white, 'white'],
+    [beige, 'beige'],
+    [purple, 'purple'],
+    [orange, 'orange'],
+];
 
-function changePurple() {
-    document.querySelector('.background-gradient').className = 'purple background-gradient'
-}
-purple.addEventListener('click', changePurple)
-
-function changeOrange() {
-    document.querySelector('.background-gradient').className = 'orange background-gradient'
-}
-orange.addEventListener('click', changeOrange)
+colors.forEach(origin=>{
+    origin[0].addEventListener('click', ()=>{
+        document.querySelector('.background-gradient').className = `background-gradient ${origin[1]} `
+    })
+})
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// let colors = [green, pink]
-
-
-
-// colors.forEach(color=>{
-//     color.addEventListener('click', console.log('ciao'))
-// })
-
-
-// function changeColor() {
-//     document.querySelector('.background-gradient').className = `${color} background-gradient`
-// }
-
-
-
-
-
-   
+  
