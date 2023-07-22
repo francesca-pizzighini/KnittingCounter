0ac1b4de48ff2f1a743784d0e4fe64238e923c9a
francesca-pizzighini <133757414+francesca-pizzighini@users.noreply.github.com>
2023-07-22 11:59:39 +0200

    color changer is working. i'm still fighting with js

---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+let blue = document.querySelector('.blue');
+let green = document.querySelector('.green')
+let pink = document.querySelector('.pink');
+let black = document.querySelector('.black');
+let white = document.querySelector('.white');
+let beige = document.querySelector('.beige');
+let purple = document.querySelector('.purple');
+let orange = document.querySelector('.orange');
+
+
+
+function changeBlue(){
+    document.querySelector('.background-gradient').className = 'blue background-gradient'
+}
+blue.addEventListener('click', changeBlue)
+
+function changeGreen() {
+    document.querySelector('.background-gradient').className = 'green background-gradient'
+}
+green.addEventListener('click', changeGreen)
+
+function changePink() {
+    document.querySelector('.background-gradient').className = 'pink background-gradient'
+}
+pink.addEventListener('click', changePink)
+
+function changeBlack() {
+    document.querySelector('.background-gradient').className = 'black background-gradient'
+}
+black.addEventListener('click', changeBlack)
+
+function changeWhite() {
+    document.querySelector('.background-gradient').className = 'white background-gradient'
+}
+white.addEventListener('click', changeWhite)
+
+
+function changeBeige() {
+    document.querySelector('.background-gradient').className = 'beige background-gradient'
+}
+beige.addEventListener('click', changeBeige)
+
+function changePurple() {
+    document.querySelector('.background-gradient').className = 'purple background-gradient'
+}
+purple.addEventListener('click', changePurple)
+
+function changeOrange() {
+    document.querySelector('.background-gradient').className = 'orange background-gradient'
+}
+orange.addEventListener('click', changeOrange)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let colors = [green, pink]
+
+
+
+// colors.forEach(color=>{
+//     color.addEventListener('click', console.log('ciao'))
+// })
+
+
+// function changeColor() {
+//     document.querySelector('.background-gradient').className = `${color} background-gradient`
+// }
+
+
+
+
+
+   
